0ab0b3efd1df9193d91f845c1ad6ca5f8d18ae72
ch huang <2942488406@qq.com>
2025-01-16 15:25:34 +0800

    优化任务三

--- task3.py
+++ task3.py
@@ -1,29 +1,11 @@
+# 只检测面积最大的标靶，面积大代表距离近，能更好地实现任务
 import numpy as np
 import cv2
 import math
 def distance(a,b):
     return ((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5
-# 待优化
-# def find_top(dots):
-#     top = dots[0]
-#     far = distance(dots[0],dots[0]) + distance(dots[0],dots[1]) 
-#     + distance(dots[0],dots[2]) + distance(dots[0],dots[3]) + distance(dots[0],dots[4])
-#     for dot in dots:
-#         far_new = distance(dot,dots[0]) + distance(dot,dots[1]) 
-#         + distance(dot,dots[2]) + distance(dot,dots[3]) + distance(dot,dots[4])
-#         if far < far_new:
-#             top = dot
-#     return top
-def find_top2(dots,center):
-    top = dots[0]
-    far = distance(dots[0],center)
-    for dot in dots:
-        far_new = distance(dot,center)
-        if far < far_new:
-            top = dot
-    return top
 
-frame = cv2.imread('pic2.jpg')
+frame = cv2.imread('pic4.png')
 (h, w) = frame.shape[:2]
 
 frame = cv2.resize(frame, (w//4, h//4))
@@ -38,30 +20,38 @@ gray = cv2.bitwise_and(gray, gray, mask=mask)
 gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
 
 contours,hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-for contour in contours:
-    
-    M = cv2.moments(contour)
-    if M["m00"] > 0:
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m01"] / M["m00"])
-        approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),closed=True)
-        frame = cv2.drawContours(frame, [approx], -1, (0,255,0), 3)
-        center = (cx, cy)
+s = [cv2.contourArea(x) for x in contours]
+contour = contours[s.index(max(s))]
+
+approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),closed=True)
+
+center,(Ma,ma),e_angle = cv2.fitEllipse(approx)
+center = tuple(map(int,center))
 
 app_tuple = [tuple(x[0]) for x in approx]
-# top = find_top(app_tuple)
-top = find_top2(app_tuple,center)
-cv2.circle(frame, (cx, cy), 5, (255, 255, 255), -1)
+dis_list = [distance(x,center) for x in app_tuple]
+
+dis_max = max(dis_list)
+top = app_tuple[dis_list.index(dis_max)]
+        
+cv2.circle(frame, center, 5, (255, 255, 255), -1)
 cv2.circle(frame, top, 5, (255, 255, 255), -1)
-cv2.line(frame, (cx, cy), top, (255, 255, 255), 3)
-basic_vertix = (cx - top[0], cy - top[1])
+cv2.line(frame, center, top, (255, 255, 255), 3)
+basic_vertix = (center[0] - top[0], center[1] - top[1])
 # nomal basic_vertix should be (0,x)
 angle = basic_vertix[1] / ((basic_vertix[0]**2 + basic_vertix[1]**2)**0.5)
 angle = math.acos(angle)
+rotated_vertix = (basic_vertix[0]*math.cos(angle) - basic_vertix[1]*math.sin(angle), basic_vertix[0]*math.sin(angle) + basic_vertix[1]*math.cos(angle)) 
+if rotated_vertix[0] != 0:
+    angle = 2 * math.pi - angle
+    angle = angle*180/math.pi
+else:
+    angle = angle*180/math.pi
+
 (h, w) = frame.shape[:2]
-m = cv2.getRotationMatrix2D((w//2,h//2), angle*180/math.pi, 1) 
+m = cv2.getRotationMatrix2D((w//2,h//2), angle, 1) 
 result = cv2.warpAffine(frame, m, (w, h))
-print(frame.shape)
+
 cv2.imshow('frame',frame)
 cv2.imshow('result',result)
 
