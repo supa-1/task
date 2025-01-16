6f6bdb1ad291575c2309053df61da777c41c509c
ch huang <2942488406@qq.com>
2025-01-16 10:50:44 +0800

    优化四，补充一

--- task1.py
+++ task1.py
@@ -1,25 +1,31 @@
 import numpy as np
 import cv2
-import time
 
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('task4_level2.mp4')
-prev_time = time.time()
+# 摄像头版
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 15)
+# 视频版
+# cap = cv2.VideoCapture('task4_level2.mp4')
+# fps = cap.get(cv2.CAP_PROP_FPS)
+# new_fps = 15
+# frame_interval = int(fps / new_fps)
+# frame_count = 0
 
 while(True):
     ret, frame = cap.read()
-    current_time = time.time()
-    fps = 1 / (current_time - prev_time)
-    prev_time = current_time
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # gray = cv2.resize(gray, (320, 320))
 
-    # 在帧上叠加帧率信息
-    cv2.putText(gray, f'FPS: {fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
+    # if frame_count % frame_interval == 0:
+    #     cv2.imshow('Frame', frame)
+    #     if cv2.waitKey(int(1000 / new_fps)) & 0xFF == ord('q'):
+    #         break
+    # frame_count += 1
+          
+    
     cv2.imshow('frame', gray)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if cv2.waitKey(5) & 0xFF == ord('q'):
         break
     
 cap.release()
